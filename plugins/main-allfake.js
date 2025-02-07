b3bbcacbaff46b6b0a2c3af54ade46222a248371
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-07 15:53:20 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -113,7 +113,7 @@ global.icono = [
 'https://files.catbox.moe/jg380e.jpg',
 'https://files.catbox.moe/629y96.jpg',
 'https://files.catbox.moe/ovdrkb.jpg',
-'https://qu.ax/QLfNz.jpg',
+'https://files.catbox.moe/y9rrvf.jpg',
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: 'sumi-Bot_MD', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
