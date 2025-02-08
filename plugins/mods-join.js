e852b3e032766dc2b0aba4e458011bf20ac1f2e5
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-07 22:18:09 -0600

    Actualizar mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -26,4 +26,7 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
 handler.help = ['join [chat.whatsapp.com]'];
 handler.tags = ['premium'];
 handler.command = /^join|nuevogrupo$/i;
+handler.private = true
+handler.group = true
 export default handler;
+
