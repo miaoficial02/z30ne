356ac29768a18614acdd42833d33c926ab004777
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-07 22:20:35 -0600

    Actualizar mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -27,6 +27,6 @@ handler.help = ['join [chat.whatsapp.com]'];
 handler.tags = ['premium'];
 handler.command = /^join|nuevogrupo$/i;
 handler.private = false;
-handler.group = false;
+handler.group = true;
 export default handler;
 
