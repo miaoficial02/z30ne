26686ec072fe83808f7629684a3cdcb69e42ee77
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-09 12:47:10 -0600

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.length < 10) return conn.reply(m.chat, '⚠️ *Especifique bien el error, mínimo 10 caracteres.*', m)
     let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
     if (text.length > 1000) return conn.reply(m.chat, '⚠️ *Máximo 1000 caracteres para enviar el error.*', m)
-    const teks = `*❌️ \`Reportes de errores\` ❌️*
+    const teks = `*✖ \`Reportes de errores\` ✖*
 
 ☁️ Número:
 • Wa.me/${m.sender.split`@`[0]}
