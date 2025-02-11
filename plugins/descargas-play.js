cdb73722c79322b741c71676cd96120dcbd07c19
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-10 21:22:54 -0600

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -114,7 +114,7 @@ let handler = async (m, { conn, args }) => {
 
 handler.help = ['play *<texto>*'];
 handler.tags = ['dl'];
-handler.command = ['play', 'play2'];
+handler.command = ['playp', 'play2p'];
 
 export default handler;
 
