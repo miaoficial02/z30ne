72d78236bbe3c52928d8ef014290a4e1ce1643d8
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-11 13:21:02 -0400

    Actualizar descargas-play (2) (1).js

--- plugins/descargas-play (2) (1).js
+++ plugins/descargas-play (2) (1).js
@@ -132,7 +132,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     const fileSizeInMB = fileSize / (1024 * 1024);
 
     const videoInfo = `
-⌘━─━─[BarbozaBot-Ai]─━─━⌘
+⌘━─━─[sumi-bot]─━─━⌘
 
 ➷ *Título⤿:* ${apiTitle}
 ➷ *Subido⤿:* ${ago}
