7d069a7e564bfa1cb44d8c73ff1711e58165dd1e
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-11 15:26:00 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -47,7 +47,9 @@ let handler = async (m, { conn, text }) => {
     // Enviar información del video con miniatura
     await conn.sendMessage(m.chat, {
       image: { url: video.thumbnail },
-      caption: `🎵 *Título:* ${video.title}\n👁️ *Vistas:* ${video.views}\n⏳ *Duración:* ${video.timestamp}\n✍️ *Autor:* ${video.author.name}`,
+      caption: `⊱ ────── {.⋅ ♫ ⋅.} ───── ⊰
+
+🎵 *Título:* ${video.title}\n👁️ *Vistas:* ${video.views}\n⏳ *Duración:* ${video.timestamp}\n✍️ *Autor:* ${video.author.name}`,
     });
 
     // Enviar solo el audio
