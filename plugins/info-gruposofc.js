2968d110f0ba405b38033742b6883c68e428325c
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-11 21:42:52 -0300

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,12 +2,12 @@ let media = './Menu.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '📇', key: m.key } })
-let str = `*🔮 GRUPOS OFICIALES*
+let str = `*GRUPOS OFICIALES*
 
-    *_╭━━━⊜ Sumi*
-  *_┃🌩❏ Hola, 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot oficial y hablar con amigos*
-*1.-*https://chat.whatsapp.com/D9hmosKv0924sPqyXeu1CU_*
+    *_╭━━━⊜Sumi*
+  *_┃🌩❏ Hola,*
+➤ *𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot oficial y hablar con amigos*
+1.-*https://chat.whatsapp.com/D9hmosKv0924sPqyXeu1CU*
 *_╰━━━━━━━━━━━━━━━━⊜_*`
 await conn.sendFile(m.chat, media, 'imagen', str, fkontak)}
 
