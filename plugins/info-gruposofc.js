aaffd3102082bf972c9dfe03a9a50bd8d89c0e69
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-11 21:33:53 -0300

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,13 +1,18 @@
+let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    await conn.sendMessage(m.chat, { react: { text: '📇', key: m.key } })
+let str = `*🔮 GRUPOS OFICIALES*
 
-let grupos =  `Hola, 
+    *_╭━━━⊜ Sumi*
+  *_┃🌩❏ Hola, 
 *➤ 𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot oficial y hablar con amigos*
-*1.-*https://chat.whatsapp.com/D9hmosKv0924sPqyXeu1CU
-`
-await m.react('🌟')
-await conn.sendFile(m.chat, grupos, fkontak, null, rcanal)}
-                      
-handler.command = ['grupos','linksk','gruposofc','gruposoficiales']
-handler.register = false
+*1.-*https://chat.whatsapp.com/D9hmosKv0924sPqyXeu1CU_*
+*_╰━━━━━━━━━━━━━━━━⊜_*`
+await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
+
+handler.command = /^grupos|linksk|gruposofc|gruposoficiales$/i
+handler.register = true
+handler.exp = 33
 
 export default handler
