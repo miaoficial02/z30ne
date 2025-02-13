1e1f0b0415d5ed4a59b6e4fe239497e4438b9b64
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-12 23:27:02 -0300

    Sumi

--- plugins/probar.js
+++ plugins/probar.js
@@ -21,29 +21,29 @@ const handler = async (m, {conn}) => {
   const fechaasi = moment().tz('Asia/Jakarta').format('DD/MM HH:mm');
   const fechabra = moment().tz('America/Sao_Paulo').format('DD/MM HH:mm');
   const fechaafri = moment().tz('Africa/Malabo').format('DD/MM HH:mm');
-  await conn.sendMessage(m.chat, {text: `\`\`\`
-「 ZONA-HORARIA ⏰ 」
-⏱️Peru       : ${fechaper}
-⏱️Mexico     : ${fechamex}
-⏱️Bolivia    : ${fechabol}
-⏱️Chile      : ${fechachi}
-⏱️Argentina  : ${fechaarg}
-⏱️Colombia   : ${fechacol}
-⏱️Ecuador    : ${fechaecu}
-⏱️Costa_Rica : ${fechacosr}
-⏱️Cuba       : ${fechacub}
-⏱️Guatemala  : ${fechagua}
-⏱️Honduras   : ${fechahon}
-⏱️Nicaragua  : ${fechanic}
-⏱️Panama     : ${fechapan}
-⏱️Uruguay    : ${fechauru}
-⏱️Venezuela  : ${fechaven}
-
-⏱️New York   : ${fechanew}
-⏱️Asia       : ${fechaasi}
-⏱️Brasil     : ${fechabra}
-⏱️G.N.Q      : ${fechaafri}
-\`\`\`
+  await conn.sendMessage(m.chat, {text: `
+「 ZONA-HORARIA  」
+Peru       : ${fechaper}
+Mexico     : ${fechamex}
+Bolivia    : ${fechabol}
+Chile      : ${fechachi}
+Argentina  : ${fechaarg}
+Colombia   : ${fechacol}
+Ecuador    : ${fechaecu}
+Costa_Rica : ${fechacosr}
+Cuba       : ${fechacub}
+Guatemala  : ${fechagua}
+Honduras   : ${fechahon}
+Nicaragua  : ${fechanic}
+Panama     : ${fechapan}
+Uruguay    : ${fechauru}
+Venezuela  : ${fechaven}
+Paraguay   : ${fechapar}
+New York   : ${fechanew}
+Asia       : ${fechaasi}
+Brasil     : ${fechabra}
+G.N.Q      : ${fechaafri}
+`
 ${String.fromCharCode(8206).repeat(850)}
 Zona horaria del servidor actual:\n[ ${Intl.DateTimeFormat().resolvedOptions().timeZone} ]\n${moment().tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format('DD/MM/YY HH:mm:ss')}`}, {quoted: m});
 };
