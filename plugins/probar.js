35c3c3ceaccc1b3c1a7e73945a7390763f7b8a0b
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-12 23:08:14 -0300

    Actualizar probar.js

--- plugins/probar.js
+++ plugins/probar.js
@@ -8,8 +8,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`${saludo}
-${username}\n*Omg llegue tarde :( culpa de*\nDiomar`);
+          m.reply(`Hola ${saludo}            ${username} *Omg llegue tarde :( culpa de*\nDiomar`);
             });
 }
 handler.help = ['info']
