d99eddd83182d1ea918f3c0394b15a43cd4de45a
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-12 23:55:03 -0300

    Actualizar Di.js

--- plugins/Di.js
+++ plugins/Di.js
@@ -1,56 +1,19 @@
-let handler = async (m, { isPrems, conn }) => {
-let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `info *${msToTime(time - new Date())}*`
+ let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `DIOMAR ES GAY`.trim()
 
-let img = 'https://telegra.ph/file/39103fc7092ca4228d253.png'
-let dia = Math.floor(Math.random() * 30)
-let tok = Math.floor(Math.random() * 10)
-let hadesb = Math.floor(Math.random() * 4000)
-let expp = Math.floor(Math.random() * 5000)
-
-  global.db.data.users[m.sender].limit += dia
-  global.db.data.users[m.sender].money += hadesb
-  global.db.data.users[m.sender].joincount += tok
-  global.db.data.users[m.sender].exp += expp
-
-let texto = `*DIOMAR EL GAY 😍*`
-
-const fkontak = {
-        "key": {
-    "participants":"0@s.whatsapp.net",
-                "remoteJid": "status@broadcast",
-                "fromMe": false,
-                "id": "Halo"
-        },
-        "message": {
-                "contactMessage": {
-                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-                }
-        },
-        "participant": "0@s.whatsapp.net"
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', 'BY RAYO', 'status@broadcast')
 }
-await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
-//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
-global.db.data.users[m.sender].lastcofre = new Date * 1
-}
-handler.help = ['daily']
-handler.tags = ['xp']
-handler.command = ['A', 'B'] 
-handler.register = true
-export default handler
+handler.command = /^(que)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
 
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+handler.admin = false
+handler.botAdmin = false
 
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+handler.fail = null
 
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " Horas " + minutes + " Minutos"
-}
+export default handler 
+ 
