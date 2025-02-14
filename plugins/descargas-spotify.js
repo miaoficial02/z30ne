15498455c925df9eb611d61d83a690d8a1d48afe
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-14 18:44:20 -0300

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,25 +1,117 @@
-import fetch from 'node-fetch'
+import axios from 'axios';
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '• Ingresa un enlace de YouTube.', m)
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-try {
-let apiUrl = `https://api.diioffc.web.id/api/download/ytmp4?url=${encodeURIComponent(text)}`
-let response = await fetch(apiUrl)
-let result = await response.json()
+    if (!text) return conn.reply(m.chat, `🪼 Por favor proporciona el nombre de una canción o artista.`, m);
 
-if (!result.status) throw new Error('No se pudo obtener el video.')
+    try {
+        let songInfo = await spotifyxv(text);
+        if (!songInfo.length) throw `No se encontró la canción.`;
+        let song = songInfo[0];
+        const res = await fetch(`https://archive-ui.tanakadomp.biz.id/download/spotify?url=${song.url}`);
 
-let { title, thumbnail, views, duration, download } = result.result
-let info = `• *Título:* ${title}\n• *Vistas:* ${views.toLocaleString()}\n• *Duración:* ${duration.timestamp}`
+        if (!res.ok) throw `Error al obtener datos de la API, código de estado: ${res.status}`;
 
-await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: info }, { quoted: m })
-await conn.sendMessage(m.chat, { video: { url: download.url }, caption: title }, { quoted: m })
+        const data = await res.json().catch((e) => { 
+            console.error('Error parsing JSON:', e);
+            throw "Error al analizar la respuesta JSON.";
+        });
 
-} catch (error) {
-console.error(error)
-conn.reply(m.chat, '❌ Error al descargar el video.', m)
-}}
+        if (!data || !data.result || !data.result.data || !data.result.data.download) throw "No se pudo obtener el enlace de descarga.";
 
-handler.command = ['ytmp4']
-export default handler
+        const info = `🪼 *Descargando:* ${data.result.data.title}\n\n🪽 *Artista:* ${data.result.data.artis}\n🪸 *Álbum:* ${song.album}\n🪷 *Duración:* ${timestamp(data.result.data.durasi)}\n⛓️‍💥 *Enlace:* ${song.url}`;
+
+        await conn.sendMessage(m.chat, { text: info, contextInfo: { forwardingScore: 9999999, isForwarded: true, 
+        externalAdReply: {
+            showAdAttribution: true,
+            containsAutoReply: true,
+            renderLargerThumbnail: true,
+            title: 'Hutao • Spotify Music',
+            body: dev,
+            mediaType: 1,
+            thumbnailUrl: data.result.data.image,
+            mediaUrl: data.result.data.download,
+            sourceUrl: data.result.data.download
+        }}}, { quoted: m });
+
+        conn.sendMessage(m.chat, { audio: { url: data.result.data.download }, fileName: `${data.result.data.title}.mp3`, mimetype: 'audio/mp4', ptt: true }, { quoted: m });
+
+    } catch (e1) {
+        m.reply(`${e1.message || e1}`);
+    }
+};
+
+handler.help = ['spotify', 'music'];
+handler.tags = ['downloader'];
+handler.command = ['spotify', 'music'];
+handler.group = true
+export default handler;
+
+async function spotifyxv(query) {
+    let token = await tokens();
+    let response = await axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/search?q=' + query + '&type=track',
+        headers: {
+            Authorization: 'Bearer ' + token,
+        },
+    });
+    const tracks = response.data.tracks.items;
+    const results = tracks.map((track) => ({
+        name: track.name,
+        artista: track.artists.map((artist) => artist.name),
+        album: track.album.name,
+        duracion: timestamp(track.duration_ms),
+        url: track.external_urls.spotify,
+        imagen: track.album.images.length ? track.album.images[0].url : '',
+    }));
+    return results;
+}
+
+async function tokens() {
+    const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
+        },
+        data: 'grant_type=client_credentials',
+    });
+    return response.data.access_token;
+}
+
+function timestamp(time) {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
+
+async function getBuffer(url, options) {
+    try {
+        options = options || {};
+        const res = await axios({
+            method: 'get',
+            url,
+            headers: {
+                DNT: 1,
+                'Upgrade-Insecure-Request': 1,
+            },
+            ...options,
+            responseType: 'arraybuffer',
+        });
+        return res.data;
+    } catch (err) {
+        return err;
+    }
+}
+
+async function getTinyURL(text) {
+    try {
+        let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
+        return response.data;
+    } catch (error) {
+        return text;
+    }
+}
