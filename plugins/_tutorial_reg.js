b28637d13f32b275fe77d3f11aa5f2db57e5337a
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-14 23:33:03 -0300

    Actualizar _tutorial_reg.js

--- plugins/_tutorial_reg.js
+++ plugins/_tutorial_reg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   const videoPath = './src/tutorial.mp4';
 
   const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Sumi-zakurazawa.`;
-await conn.sendButton(m.chat, texto, wm, img, [['Menu ☄', '/menu'] ], fkontak, m)  
+await conn.sendButton(m.chat, texto, wm, img, [['Menu ☄', '/menu'] ], rcanal, m)  
   const options = {
     quoted: m,
     caption: texto,
