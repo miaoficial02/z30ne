cf64953bdbe9d38b3f0e4fb0ce9641bfb119b528
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-14 20:10:51 -0600

    Actualizar owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,7 +1,12 @@
+//Codigo por Zahpkiel Ender
+
+
+
+
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*[ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '❤️‍🔥 Sumi sakurasaza', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/584164137403'}}})
+conn.reply(m.chat, `> [ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion` ,m, { contextInfo:{ externalAdReply: {title: '❤️‍🔥 Sumi sakurasaza', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/584164137403'}}})
 }
 handler.customPrefix = /@584164137403/i;
 handler.command = new RegExp();
