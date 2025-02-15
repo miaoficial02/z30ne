0fda1489fe113db67247efabdd1f6c520430e953
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-15 10:26:04 -0600

    Actualizar plugins

---
+++ plugins/info-instalarbot.js
@@ -0,0 +1,60 @@
+//Hecho zaphkiel
+
+
+
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+var handler = async (m, { conn }) => {
+    let texto = `╭─────────────┈⊷
+│ *INSTALACIÓN DE SUMI-ZAKURASAWA*
+╰┬────────────┈⊷
+┌┤ *REQUISITOS PARA LA INSTALACIÓN*
+┌┤➳ _Dudas: wa.me/51929972576_
+┌┤➳ _1 GB de almacenamiento_
+┌┤➳ _Termux: https://www.mediafire.com/file/3hsvi3xkpq3a64o/termux_118.apk/file_
+┌┤➳ _GitHub: https://github.com/leoneloficial/Sumi-zakurasawa_
+┌┤➳ _Un WhatsApp inmune (secundario)_
+┌┤➳ _Un número virtual_
+┌┤➳ _2 dispositivos o una PC para escanear_
+╰┬────────────┈⊷
+  │ *COMANDOS DE INSTALACIÓN EN TERMUX*
+  │> termux-setup-storage
+  │> apt update -y && apt upgrade -y
+  │> pkg install -y git nodejs ffmpeg imagemagick yarn
+  │> git clone https://github.com/leoneloficial/Sumi-zakurasawa && cd Sumi-zakurasawa
+  │> bash install.sh
+  │> npm start
+  ╭──────────────────────────────────
+  │ Escribe "instalarbot" para enviar estos comandos automáticamente.
+  ╰────────────┈⊷`;
+
+    conn.reply(m.chat, texto, m);
+};
+
+async function enviarComandos(m, comandos) {
+    for (let cmd of comandos) {
+        await delay(1500);
+        m.reply(cmd);
+    }
+}
+
+handler.before = async (m) => {
+    if (/^instalarbot$/i.test(m.text)) {
+        let comandos = [
+            'termux-setup-storage',
+            'apt update -y && apt upgrade -y',
+            'pkg install -y git nodejs ffmpeg imagemagick yarn',
+            'git clone https://github.com/leoneloficial/Sumi-zakurasawa && cd Sumi-zakurasawa',
+            'bash install.sh',
+            'npm start'
+        ];
+        await enviarComandos(m, comandos);
+    }
+};
+
+handler.help = ['instalarbot'];
+handler.tags = ['info'];
+handler.command = /^instalarbot$/i;
+
+export default handler;
