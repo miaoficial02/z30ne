d4bf6149b6df92cd0edcdd41670f773a6395bd3e
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-14 23:32:28 -0300

    Sumi Sakurasawa

--- plugins/_tutorial_reg.js
+++ plugins/_tutorial_reg.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
   const usuario = m.pushName || 'Usuario';
   const videoPath = './src/tutorial.mp4';
 
-  const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Megumin-Bot.`;
-
+  const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Sumi-zakurazawa.`;
+await conn.sendButton(m.chat, texto, wm, img, [['Menu ☄', '/menu'] ], fkontak, m)  
   const options = {
     quoted: m,
     caption: texto,
